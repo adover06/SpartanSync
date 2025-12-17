806589a8e3b55b272ecd21d1e3e9a78222d9a2e1
Andrew Dover <monkeypunk811@gmail.com>
2025-12-16 21:45:26 -0800

    Added fallback if openai key is not provided

--- app/main/gpt_client.py
+++ app/main/gpt_client.py
@@ -11,14 +11,18 @@ SYSTEM_PROMPT = (
     "You need to provide a simple plan for completing them all on time, this means you should also include a short few advice section at the end on how to stay organized and manage time effectively. Also ensure to reference the apps name 'SpartanSync'"
     "Use emojis and keep it concise. Also make sure to create new lines when needed rather than one long paragraph."
 )
-
 def ask_chatgpt(comments: str, assignments: str) -> str:
-
-    response = client.chat.completions.create(
-        model="gpt-4.1-mini",
-        messages=[
-            {"role":"system", "content": SYSTEM_PROMPT},
-            {"role":"user", "content": f"{comments}\n\nAssignments:\n{assignments}"}
-        ]
-    )
-    return response.choices[0].message.content
+    if not os.getenv("OPENAI_API_KEY"):
+        return "Warning: Contact your administrator. OpenAI features are disabled until key is setup."
+    else:
+        try:
+            response = client.chat.completions.create(
+                model="gpt-4.1-mini",
+                messages=[
+                    {"role": "system", "content": SYSTEM_PROMPT},
+                    {"role": "user", "content": f"{comments}\n\nAssignments:\n{assignments}"}
+                ]
+            )
+            return response.choices[0].message.content
+        except Exception as e:
+            return f"Warning: ChatGPT request failed: {e}"
